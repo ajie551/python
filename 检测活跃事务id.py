8c7e415f5b6194614ba9e16782063ea852a1f4db
ajie551 <42767709+ajie551@users.noreply.github.com>
2020-07-02 20:23:30 +0800

    Update 检测活跃事务id.py

--- 检测活跃事务id.py
+++ 检测活跃事务id.py
@@ -1,12 +1,16 @@
 # 检测活跃事务id,如有事务持续5s以上未结束.则发送警报
-import MySQLdb, json, urllib.request
+import json
 import time
+import urllib.request
+from collections import Counter  # 引入Counter
+
+import MySQLdb
 
 
 def send_dingding(data_Content):
     # 1、构建url
     url = "https://oapi.dingtalk.com/robot/send?access_token=3d193af514e746a68b9b9e4b4cf1435a9dc125ff20fd792c8539d3baddb24476"  # url为机器人的webhook
-
+    
     # 2、构建一下请求头部
     header = {
         "Content-Type": "application/json",
@@ -25,16 +29,16 @@ def send_dingding(data_Content):
             "isAtAll": False  # @全体成员（在此可设置@特定某人）
         }
     }
-
+    
     # 4、对请求的数据进行json封装
     sendData = json.dumps(data)  # 将字典类型数据转化为json格式
     sendData = sendData.encode("utf-8")  # python3的Request要求data为byte类型
-
+    
     # 5、发送请求
     request = urllib.request.Request(url=url, data=sendData, headers=header)
     #
     # 6、将请求发回的数据构建成为文件格式
-
+    
     opener = urllib.request.urlopen(request)
     # 7、打印返回的结果
     # print(opener.read())
@@ -42,6 +46,7 @@ def send_dingding(data_Content):
 
 while True:
     count = 0
+    j = []
     while count < 3:
         db = MySQLdb.connect("rm-2zez7u673640b68x1.mysql.rds.aliyuncs.com", "zhwdb", "3NPjzZ14YBGnCo0d",
                              "information_schema",
@@ -49,20 +54,22 @@ while True:
         cursor = db.cursor()
         cursor.execute("select trx_id from  INNODB_TRX;")
         trx_id = cursor.fetchall()
-        j = set()
         if len(trx_id) == 0:  # 判断事务id是否为空,为空则中断进入下一次循环
             count = 0
+            j = []
             time.sleep(2)
         else:
             for i in trx_id:  # 不为空将事务id放入一个集合中,首次集合与自身相交,二次与新集合相交,取出持续存在的事务id
-                j.add(i[0])
-                s = j
-            j = j & s
-            count += 1
-            print(count)
-            time.sleep(2)
-    if len(j) > 0:  # 5s内交集集合长度大于0时,发送警告信息
-        send_dingding('事务持续5s未结束,id为{}'.format(j))
+                j.append(i[0])
+                count+=1
+        b = dict(Counter(j))
+        print(b)
+        s = [key for key, value in b.items() if value > 1]
+        print(s)
+        time.sleep(2)
+    if len(s) > 0:  # 5s内交集集合长度大于0时,发送警告信息
+        #send_dingding('事务持续5s未结束,id为{}'.format(s))
+        print("报错")
         time.sleep(10)
     else:
         db.close()
