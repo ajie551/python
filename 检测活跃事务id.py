846ac3a435507ffab0200e2cc69e8e5575d02a75
ajie551 <42767709+ajie551@users.noreply.github.com>
2020-07-02 18:25:24 +0800

    Update 检测活跃事务id.py

--- 检测活跃事务id.py
+++ 检测活跃事务id.py
@@ -1,11 +1,11 @@
-#检测活跃事务id,如有事务持续5s以上未结束.则发送警报
+# 检测活跃事务id,如有事务持续5s以上未结束.则发送警报
 import MySQLdb, json, urllib.request
 import time
 
 
 def send_dingding(data_Content):
     # 1、构建url
-    url = "xxxxxxx"  # url为机器人的webhook
+    url = "https://oapi.dingtalk.com/robot/send?access_token=3d193af514e746a68b9b9e4b4cf1435a9dc125ff20fd792c8539d3baddb24476"  # url为机器人的webhook
 
     # 2、构建一下请求头部
     header = {
@@ -20,7 +20,7 @@ def send_dingding(data_Content):
         },
         "at": {
             "atMobiles": [
-                "xxxxxx手机号",
+                "18310699089",
             ],
             "isAtAll": False  # @全体成员（在此可设置@特定某人）
         }
@@ -43,24 +43,25 @@ def send_dingding(data_Content):
 while True:
     count = 0
     while count < 3:
-        db = MySQLdb.connect("xxxxxx", "xxxxx", "xxxxxxx",
+        db = MySQLdb.connect("rm-2zez7u673640b68x1.mysql.rds.aliyuncs.com", "zhwdb", "3NPjzZ14YBGnCo0d",
                              "information_schema",
                              charset='utf8')
         cursor = db.cursor()
         cursor.execute("select trx_id from  INNODB_TRX;")
         trx_id = cursor.fetchall()
-        j= set()#定义空集合存储5s内事务id的交集
-        print(trx_id)
-        if len(trx_id) == 0:#判断事务id是否为空,为空则中断进入下一次循环
+        j = set()
+        if len(trx_id) == 0:  # 判断事务id是否为空,为空则中断进入下一次循环
+            count = 0
             time.sleep(2)
-            continue
         else:
-            for i in trx_id:#不为空将事务id放入一个集合中,首次集合与自身相交,二次与新集合相交,取出持续存在的事务id
+            for i in trx_id:  # 不为空将事务id放入一个集合中,首次集合与自身相交,二次与新集合相交,取出持续存在的事务id
                 j.add(i[0])
-            j = j & j
+                s = j
+            j = j & s
             count += 1
+            print(count)
             time.sleep(2)
-    if len(j) > 0:#5s内交集集合长度大于0时,发送警告信息
+    if len(j) > 0:  # 5s内交集集合长度大于0时,发送警告信息
         send_dingding('事务持续5s未结束,id为{}'.format(j))
         time.sleep(10)
     else:
